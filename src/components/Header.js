f916b67a8f1fbe407ad0100842842248724ba9d3
Jade <jade.arevort@gmail.com>
2022-07-01 02:48:42 -0300

    comienzo

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../images/logo.png';
+
+function Header (){
+
+    return (
+
+        <>
+        
+        </>
+
+    )
+}
