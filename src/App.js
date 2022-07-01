b2512096271ae2febd21d6400935b92371a19a6c
Jade <jade.arevort@gmail.com>
2022-07-01 10:17:57 -0300

    parte 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from '../Header';
+import Banner from '../Banner';
 
 function App() {
   return (
---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Principal from '../images/principal.jpeg';
+
+
+function Banner (){
+return (
+
+        <>
+
+<div class="container"/>
+    <div class="row my-4"/>
+      <div class="col-md-12"/>
+        <img class="img-fluid box-shadow" src={Principal} alt="Campus Almagro ORT"/>
+      <div/>
+    <div/>
+
+    </>
+)
+
+}
+
+
+export default Banner;
